c9b5c211f7865cf69600f8af51397892e3921d5e
Luca <lucab@MacBook-Pro-di-Luca-4.local>
2023-11-17 15:57:52 +0100

    Added home, login and signup page to Home Servlet

--- fractal-tris/src/main/java/fttt/fractaltris/HomeServlet.java
+++ fractal-tris/src/main/java/fttt/fractaltris/HomeServlet.java
@@ -1,13 +1,14 @@
 package fttt.fractaltris;
 
 import java.io.*;
+import java.util.Objects;
 
 import fttt.fractaltris.htmlBuilder.HTMLBuilder;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 
-@WebServlet(name = "Home-servlet", value = "/home")
+@WebServlet(name = "Home-servlet", urlPatterns = {"/home", "/home/login", "/home/signup"})
 public class HomeServlet extends HttpServlet {
     HTMLBuilder pagerBuilder;
     public void init() {
@@ -16,10 +17,16 @@ public class HomeServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
         PrintWriter out = response.getWriter();
 
-        out.println(pagerBuilder.getHTMLFile("home.html"));
+        String[] urlPath = request.getServletPath().split("/");
+        if(urlPath.length == 2){
+            out.println(pagerBuilder.getHTMLFile("home.html"));
+        } else if(urlPath[2].equals("login")){
+            out.println(pagerBuilder.getHTMLFile("login.html"));
+        } else if(urlPath[2].equals("signup")){
+            out.println(pagerBuilder.getHTMLFile("signup.html"));
+        }
     }
 
     public void destroy() {
